235b8c3c24b0c090fa662148eae30227f067b7a1
Luciano <lucianoprk@gmail.com>
2018-09-29 20:53:36 -0300

    correção utf-8

--- Library/Database.php
+++ Library/Database.php
@@ -1,64 +1,53 @@
 <?php
 
-class Database {
-
+class Database{
+    # Variável que guarda a conexão PDO.
     protected static $db;
-
-    private function __construct() {
+    # Private construct - garante que a classe só possa ser instanciada internamente.
+    private function __construct(){
+        # Informações sobre o banco de dados:
         $db_host = "localhost";
-        $db_nome = "db_cultura";
-        $db_usuario = "postgres";
+        $db_nome = "CdoC";
+        $db_usuario = "root";
         $db_senha = "infodmz626";
-        $db_driver = "pgsql";
+        $db_driver = "mysql";
         # Informações sobre o sistema:
         $sistema_titulo = "Cultura do Campo";
         $sistema_email = "app.culturadocampo@gmail.com";
-
-        try {
+       
+        try{
             # Atribui o objeto PDO à variável $db.
             self::$db = new PDO("$db_driver:host=$db_host; dbname=$db_nome", $db_usuario, $db_senha);
-            # Garante que o PDO lance exce��es durante erros.
+            # Garante que o PDO lance exceções durante erros.
             self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            # Garante que os dados sejam armazenados com codifica��o UFT-8.
-            self::$db->exec();
-        } catch (PDOException $e) {
-            # Envia um e-mail para o e-mail oficial do sistema, em caso de erro de conex�o.
-          //  mail($sistema_email, "PDOException em $sistema_titulo", $e->getMessage());
+            # Garante que os dados sejam armazenados com codificação UFT-8.
+            self::$db->exec('SET NAMES utf8');
+        }
+        catch (PDOException $e){
+            # Envia um e-mail para o e-mail oficial do sistema, em caso de erro de conexão.
+            mail($sistema_email, "PDOException em $sistema_titulo", $e->getMessage());
             # Então não carrega nada mais da página.
             die("Connection Error: " . $e->getMessage());
         }
     }
-
-    # M�todo est�tico - acess�vel sem instancia��o.
-
-    public static function conexao() {
-        # Garante uma �nica instância. Se não existe uma conexão, criamos uma nova.
-        if (!self::$db) {
+    # Método estático - acessível sem instanciação.
+    public static function conexao(){
+        # Garante uma única instância. Se não existe uma conexão, criamos uma nova.
+        if (!self::$db){
             new Database();
         }
         # Retorna a conexão.
         return self::$db;
     }
-
-    public static function fetch($query) {
+    public static function fetch($query){
         $db = Database::conexao();
-        $db = $db->query($query);
-        if ($db->fetch()) {
+        $db = $db->query($query);  
+        if($db->fetch()){
             return $db->fetch();
-        } else {
+        }else{
             return false;
         }
     }
-
 }
 
-
-//    public static function fetch($query) {
-//        $db = Database::conexao();
-//        $db = $db->query($query);
-//        if ($db->fetch()) {
-//            return $db->fetch();
-//        } else {
-//            return false;
-//        }
-//    }
+ 
