f1d80f4cb8aed2f17e4369611ba97db18e53974c
Willian <willianpestana@gmail.com>
2018-10-15 00:34:52 -0300

    Alterações na matriz

--- Public/Matriz/base_login.php
+++ Public/Matriz/base_login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include 'Core/Views/Base/Includes/b_head.php'; ?>
+    <?php include 'Public/Matriz/Includes/b_head.php'; ?>
     <body class="content-menu pace-done content-menu-close">
         <div id="app">
             <div class="content-wrapper">
@@ -9,6 +9,8 @@
                 </div>
             </div>
         </div>
-        <?php include 'Core/Views/Base/Includes/b_scripts.php'; ?>
+        <?php include 'Public/Matriz/Includes/b_scripts.php'; ?>
+        <script async defer src="https://apis.google.com/js/api.js" onload="this.onload=function(){};HandleGoogleApiLibrary()" onreadystatechange="if (this.readyState === 'complete') this.onload()"></script>
+
     </body>
 </html>
