d13a57645c6f800ef22f98adfaee2404777a0c59
Willian <willianpestana@gmail.com>
2018-11-24 00:05:15 -0200

    Faltou classe active no menu (temporário)

--- Public/Matriz/Sistema/menu.php
+++ Public/Matriz/Sistema/menu.php
@@ -5,7 +5,7 @@
                 <button class="m-aside-header-menu-mobile-close  m-aside-header-menu-mobile-close--skin-light" id="m_aside_header_menu_mobile_close_btn"><i class="la la-close"></i></button>
                 <div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas  m-header-menu--skin-dark m-header-menu--submenu-skin-light m-aside-header-menu-mobile--skin-light m-aside-header-menu-mobile--submenu-skin-light">
                     <ul class="m-menu__nav  m-menu__nav--submenu-arrow">
-                        <li class="m-menu__item  m-menu__item--submenu m-menu__item--tabs" m-menu-submenu-toggle="tab">
+                        <li class="m-menu__item--active-tab m-menu__item  m-menu__item--submenu m-menu__item--tabs" m-menu-submenu-toggle="tab">
                             <a href="javascript:;" class="m-menu__link m-menu__toggle" title=""><span class="m-menu__link-text">Sistema</span><i
                                     class="m-menu__hor-arrow la la-angle-down"></i><i class="m-menu__ver-arrow la la-angle-right"></i></a>
                             <div class="m-menu__submenu m-menu__submenu--classic m-menu__submenu--left m-menu__submenu--tabs">
@@ -17,7 +17,7 @@
                                         <div class="m-menu__submenu m-menu__submenu--classic m-menu__submenu--left">
                                             <span class="m-menu__arrow m-menu__arrow--adjust" style="left: 70.5px;"></span>
                                             <ul class="m-menu__subnav">
-                                                <li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true" style="background: #eee">
+                                                <li class="m-menu__item " m-menu-link-redirect="1" aria-haspopup="true">
                                                     <a href="sistema/rotas-de-acesso/adicionar" class="m-menu__link">
                                                         <i class="m-menu__link-icon flaticon-add"></i>
                                                         <span class="m-menu__link-text">Adicionar</span>
