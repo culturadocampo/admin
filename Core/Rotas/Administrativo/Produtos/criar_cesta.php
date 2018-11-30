b256151f0058aed7e4a343648b6cfa6eb60a1ce4
Willian <willianpestana@gmail.com>
2018-11-29 23:20:02 -0200

    Alteração css da interface da cesta

--- Core/Rotas/Administrativo/Produtos/criar_cesta.php
+++ Core/Rotas/Administrativo/Produtos/criar_cesta.php
@@ -4,17 +4,23 @@
     }
 
     .m-widget28 table {
-        margin-top: 16px;
+        margin-top: 72px;
     }
     .m-widget28 td div {
         height: 100px;
         line-height: 100px;
         border: 1px dashed #a0a0a0;
-    }
-    .add_icon{
-        opacity: 0.75;
+
     }
 
+    .m-widget28 td .add {
+        opacity: 0.5;
+
+    }
+
+    .m-widget28 td .add:hover {
+        opacity: 1.0;
+    }
     .m-widget28 input {
         border-radius: 0;
         border: 0;
@@ -26,19 +32,27 @@
 
 </style>
 <div class="m-grid__item m-grid__item--fluid m-wrapper" > 
-
     <div class="row">
         <div class="col-md-4">
-            <!--begin:: Widgets/Blog-->
             <div class="m-portlet m-portlet--head-overlay m-portlet--full-height   m-portlet--rounded-force">
-                <div class="m-portlet__head m-portlet__head--fit">
+                <div class="m-portlet__head">
                     <div class="m-portlet__head-caption">
-                        <div class="m-portlet__head-title">				
-                            <h3 class="m-portlet__head-text m--font-light">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Criar cesta
                             </h3>
                         </div>
                     </div>
+                    <div class="m-portlet__head-tools">
+                        <ul class="nav nav-pills nav-pills--brand m-nav-pills--align-right m-nav-pills--btn-pill m-nav-pills--btn-sm" role="tablist">
+                            <li class="nav-item m-tabs__item">
+                                <a class="btn btn-sm btn-outline-success" data-toggle="tab" href="#m_widget2_tab1_content" role="tab">
+                                    Confirmar
+                                </a>
+                            </li>
 
+                        </ul>
+                    </div>
                 </div>
                 <div class="m-portlet__body">
                     <div class="m-widget28">
@@ -47,31 +61,31 @@
                             <table style="width: 100%">
                                 <tr>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>
                                     </td>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>                                    </td>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>                                    </td>
                                 </tr>
                                 <tr>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>                                    </td>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>                                    </td>
                                     <td class="text-center">
-                                        <div>
-                                            <img class="add_icon" src="Public/Images/add.png" width="48px" height="48px">
+                                        <div class="add">
+                                            <img src="Public/Images/add.png" width="48px" height="48px">
                                         </div>                                    </td>
                                 </tr>
                             </table>
@@ -84,21 +98,22 @@
                                 <div id="menu11" class="m-widget28__tab-container tab-pane active">
                                     <div class="m-widget28__tab-items">
                                         <div class="m-widget28__tab-item">
-                                            <span>Company Name</span>
+                                            <span>Descrição</span>
                                             <input class="form-control m-input" type="text" placeholder="Digite aqui" name="username" autocomplete="off">
                                         </div>
                                         <div class="m-widget28__tab-item">
                                             <span>Valor</span>
                                             <input class="form-control m-input" type="text" placeholder="Digite aqui" name="username" autocomplete="off">
                                         </div>
-                                       
+
                                     </div>					      	 		      	
                                 </div>
 
 
                             </div>
                             <!-- end::Tab Content --> 	
-                        </div>				 	 
+                        </div>	
+
                     </div>
                 </div>
             </div>
