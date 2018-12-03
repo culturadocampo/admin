ad89bb572e480fabdef43c0a27ad9b5d86ff47e1
Fernando <fernando.h.malaquias@gmail.com>
2018-12-03 02:33:17 -0200

    Classe Anúncio

---
+++ Core/Models/Anuncio.php
@@ -0,0 +1,105 @@
+<?php
+
+class Anuncio {
+
+    private $quantidade;
+    private $valor;
+    private $visivel;
+    private $ativo;
+
+    function insert_anuncio($fk_produtor, $fk_produto) {
+        $query = "
+            INSERT INTO anuncios
+             (
+                fk_produtor, 
+                fk_produto, 
+                quantidade,
+                valor, 
+                descricao, 
+                visivel,
+                ativo
+            )
+            VALUES (
+                '{$fk_produtor}',
+                '{$fk_produto}',
+                '{$this->get_quantidade()}',
+                '{$this->get_valor()}',
+                '{$this->get_descricao()}',
+                '{$this->get_visivel()}',
+                '{$this->get_ativo()}'
+             )";
+        DATABASE::execute($query);
+    }
+
+    public function anuncios_ativos() {
+        $query = "
+            SELECT 
+                * 
+            FROM 
+                anuncios
+            WHERE 
+                TRUE 
+            AND ativo = 1   
+        ";
+        return DATABASE::fetch_all($query);
+    }
+
+    public function meus_anuncios() {
+        $query = "
+            SELECT 
+                * 
+            FROM 
+                anuncios
+        ";
+        return DATABASE::fetch_all($query);
+    }
+
+    function get_quantidade() {
+        return $this->quantidade;
+    }
+
+    function get_valor() {
+        return $this->valor;
+    }
+
+    function get_descricao() {
+        return $this->descricao;
+    }
+
+    function get_visivel() {
+        return $this->visivel;
+    }
+
+    function get_ativo() {
+        return $this->ativo;
+    }
+
+    function get_data_cadastro() {
+        return $this->data_cadastro;
+    }
+
+    function set_quantidade($quantidade) {
+        if ($quantidade) {
+            $this->quantidade = STRINGS::limpar($quantidade);
+        } else {
+            APP::return_response(false, "Favor informar a quantidade");
+        }
+    }
+
+    function set_valor($valor) {
+        if ($valor) {
+            $this->valor = STRINGS::limpar($valor);
+        } else {
+            APP::return_response(false, "Favor informar o valor");
+        }
+    }
+
+    function set_visivel($visivel) {
+        $this->visivel = STRINGS::limpar($visivel);
+    }
+
+    function set_ativo($ativo) {
+        $this->ativo = STRINGS::limpar($ativo);
+    }
+
+}
