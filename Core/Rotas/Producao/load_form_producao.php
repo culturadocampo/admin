df433e350443cccd92ce73beced4039d55b7cd5b
Willian <willianpestana@gmail.com>
2019-03-06 08:55:55 -0300

    Datapicker na produção

--- Core/Rotas/Producao/load_form_producao.php
+++ Core/Rotas/Producao/load_form_producao.php
@@ -20,19 +20,10 @@ $unidades = $o_produto->select_unidades_medida();
         </div>
     </div>	 
     <div class="form-group m-form__group row">
-
-        <div class="col-md-8">
-            <label for="quantidade">Quantidade</label>
-            <div class="m-input-icon m-input-icon--right">
-                <input name="quantidade" type="text" class="form-control m-input" placeholder="Quantidade do produto escolhido">
-                <!--<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>-->
-            </div>
-            <span class="m-form__help text-info">Relacionado à medida ao lado</span>
-        </div>
         <div class="col-md-4">
             <label for="usuario">Usuário de acesso</label>
             <div class="m-input-icon m-input-icon--right">
-                <select name='id_unidade' data-live-search="true" class="form-control selectpicker">
+                <select id="unidade_medida" name='id_unidade' data-live-search="true" class="form-control selectpicker">
                     <option selected>[Selecione a unidade]</option>
                     <?php if ($produtos) { ?>
                         <?php foreach ($unidades as $value) { ?>
@@ -42,27 +33,34 @@ $unidades = $o_produto->select_unidades_medida();
                 </select>
                 <!--<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-user"></i></span></span>-->
             </div>
-            <span class="m-form__help">Somente letras e números. Sem espaço.</span>
+            <span class="m-form__help">Unidade de medida</span>
         </div>
+        <div class="col-md-8">
+            <label for="quantidade">Quantidade</label>
+            <div class="m-input-icon m-input-icon--right">
+                <input name="quantidade" type="text" class="form-control m-input" placeholder="Quantidade do produto escolhido">
+                <!--<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>-->
+            </div>
+            <span class="m-form__help text-info">Relacionado à unidade de medida escolhida</span>
+        </div>
+
     </div>
+
     <div class="form-group m-form__group row">
 
-        <div class="col-md-6">
-            <label for="periodo_inicial">Disponibilidade inicial</label>
-            <div class="m-input-icon m-input-icon--right">
-                <input name="periodo_inicial" type="text" class="form-control m-input data_mes_ano" placeholder="00/0000">
-                <!--<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>-->
+        <div class="col-md-12">
+            <label for="periodo_final">Período de disponibilidade do produto</label>
+            <div class="input-daterange input-group">
+                <input readonly="" type="text" class="form-control m-input text-right" name="periodo_inicial" placeholder="Data inicial">
+                <div class="input-group-append">
+                    <span class="input-group-text">até</span>
+                </div>
+                <input readonly="" type="text" class="form-control m-input text-left" name="periodo_final" placeholder="Data final">
             </div>
-            <span class="m-form__help text-info">A partir de quando ficará disponível</span>
+            <!--<span class="m-form__help">Escolha uma data inicial e uma data final</span>-->
         </div>
-        <div class="col-md-6">
-            <label for="periodo_final">Disponibilidade final</label>
-            <div class="m-input-icon m-input-icon--right">
-                <input name="periodo_final" type="text" class="form-control m-input data_mes_ano" placeholder="00/0000">
-                <!--<span class="m-input-icon__icon m-input-icon__icon--right"><span><i class="la la-bookmark-o"></i></span></span>-->
-            </div>
-            <span class="m-form__help text-info">Quando este produto não estará mais disponível</span>
-        </div>
+
+
     </div>
 </div>
 
@@ -70,6 +68,14 @@ $unidades = $o_produto->select_unidades_medida();
 <script>
     $(document).ready(function () {
         $('.selectpicker').selectpicker();
-        $('.data_mes_ano').mask("00/0000");
+
+        $('input[name=periodo_inicial]').datepicker({
+            format: "dd/mm/yyyy",
+            orientation: "bottom right"
+        });
+        $('input[name=periodo_final]').datepicker({
+            format: "dd/mm/yyyy",
+            orientation: "bottom left"
+        });
     });
 </script>
