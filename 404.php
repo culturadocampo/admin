e42d22a1a0d0d0ebd4dce3080b160c33b452adcc
Willian <willianpestana@gmail.com>
2019-03-06 23:08:40 -0300

    "404" consertado

--- 404.php
+++ 404.php
@@ -6,13 +6,14 @@
         <?php } ?>
         <meta charset="utf-8">
 
-        <title>Agribox | 404</title>
+        <title>Cultura do campo</title>
         <meta name="description" content="Latest updates and statistic charts"> 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 
-        <!--begin::Web font -->
         <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7CRoboto:300,400,500,600,700" media="all"><script>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7CRoboto:300,400,500,600,700" media="all">
+        <!--begin::Web font -->
+        <script>
             WebFont.load({
                 google: {"families": ["Poppins:300,400,500,600,700", "Roboto:300,400,500,600,700"]},
                 active: function () {
@@ -23,9 +24,9 @@
         <!--end::Web font -->
 
         <!--begin::Global Theme Styles -->
-        <link href="Metronic/assets/vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css" />
-        <link href="Metronic/assets/demo/demo10/base/style.bundle.css" rel="stylesheet" type="text/css" />
-        <link rel="shortcut icon" href="Public/Images/logo_letra_c.png" />
+        <link href="Public/Vendors/base/vendors.bundle.css" rel="stylesheet" type="text/css">
+        <link href="Public/Styles/style.bundle.min.css" rel="stylesheet" type="text/css">
+        <link rel="shortcut icon" href="Public/Images/Logo/logo_modern2.png" />
 
     </head>
     <!-- end::Head -->
@@ -34,13 +35,10 @@
     <!-- begin::Body -->
     <body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--fixed m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default">
 
-
-
-        <!-- begin:: Page -->
         <div class="m-grid m-grid--hor m-grid--root m-page">
 
 
-            <div class="m-grid__item m-grid__item--fluid m-grid  m-error-5" style="background-image: url(Metronic/assets/app/media/img//error/bg5.jpg);">
+            <div class="m-grid__item m-grid__item--fluid m-grid  m-error-5" style="background-image: url(Public/Images/Outros/bg5.jpg);">
                 <div class="m-error_container">
                     <span class="m-error_title">
                         <h1>404!</h1>		 
